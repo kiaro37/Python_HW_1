e562bfa28689a5a066ca6af3394bcc0188e1f1c1
kiaro37 <iva.sakharov.e@gmail.com>
2023-03-30 12:13:41 +0300

    Task 18 complete

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
-# Задача 16: Требуется вычислить, сколько раз встречается некоторое число X в массиве A.
+# Задача 18: Требуется найти в массиве A[1..N] самый близкий по величине элемент к заданному числу X.
 # Пользователь в первой строке вводит натуральное число N – количество элементов в массиве.
-# В последующих  строках записаны N целых чисел Ai. Последняя строка содержит число X.
-# Попробуйте использовать метод count(), а также решите задачу с помощью своего алгоритма (без count).
-# Замерьте время работы двух алгоритмов и сравните, подумайте, почему оно отличается.
+# В последующих  строках записаны N целых чисел Ai. Последняя строка содержит число X
 #
 # *Пример:*
 #
 # 5
 #     1 2 3 4 5
-#     3
-#     -> 1
+#     6
+#     -> 5
 
 input_list = []
 list_len = int(input("Введите количество элементов в массиве: "))
@@ -20,20 +18,14 @@ for _ in range(1):
 print(input_list)
 x: int = input_list[list_len-1]
 
-import time
-start = time.time()
-totalizer: int = 0
-for i in range(list_len):
-    if input_list[i] == x:
-        totalizer += 1
-print(totalizer)
-end = time.time()
-print("The time of execution of above program is :", (end-start) * 10**3, "ms")
+input_list = set(input_list)
+print(input_list)
+input_list = list(input_list)
+print(input_list)
+y: int
+for i in range(len(input_list)):
+    if x == input_list[i]:
+        y = input_list[i-1]
+print(y)
 
-import time
-start = time.time()
-count = input_list.count(x)
-print(count)
-end = time.time()
-print("The time of execution of above program is :", (end-start) * 10**3, "ms")
 
