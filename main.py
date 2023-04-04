d299d760b0f7057274d513927e756948dae66c67
kiaro37 <iva.sakharov.e@gmail.com>
2023-04-04 20:38:56 +0300

    HomeWork_6 complete

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 # Задача 32: Определить индексы элементов массива (списка), значения которых принадлежат заданному
 # диапазону (т.е. не меньше заданного минимума и не больше заданного максимума)
 
+def index_array(int_arr: list):
+    len_list = int(input("Введите количество жлементов: "))
+    new_list = []
+    for i in range(len_list):
+        int_arr.append(int(input(f"Введите {i+1} элемент: ")))
+
+    min_ind = int(input("Введите минимальный индекс: "))
+    max_ind = int(input("Введите максимальный индекс: "))
+
+    print(int_arr)
+
+    for i in range(len_list):
+        if i > min_ind and i < max_ind:
+            new_list.append(i)
+
+    print(new_list)
+
+
+
+input_list: list = []
+index_array(input_list)
