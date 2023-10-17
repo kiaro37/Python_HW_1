f82bc52d1e5589294fe335301efe682007e00046
kiaro37 <iva.sakharov.e@gmail.com>
2023-10-17 16:58:25 +0300

    HomeWork 8 complete

--- main.py
+++ main.py
@@ -1,28 +1,163 @@
-# Задача 36: Напишите функцию print_operation_table(operation, num_rows=6, num_columns=6),
-# которая принимает в качестве аргумента функцию, вычисляющую элемент по номеру строки и столбца.
-# Аргументы num_rows и num_columns указывают число строк и столбцов таблицы, которые должны быть распечатаны.
-# Нумерация строк и столбцов идет с единицы (подумайте, почему не с нуля).
-# Примечание: бинарной операцией называется любая операция, у которой ровно два аргумента,
-# как, например, у операции умножения.
-#
-# *Пример:*
-#
-# **Ввод:** `print_operation_table(lambda x, y: x * y) `
-# **Вывод:**
-# 1 2 3 4 5 6
-#
-# 2 4 6 8 10 12
-# 3 6 9 12 15 18
-# 4 8 12 16 20 24
-# 5 10 15 20 25 30
-# 6 12 18 24 30 36
+# Задача 38: Дополнить телефонный справочник возможностью изменения и удаления данных.
+# Пользователь также может ввести имя или фамилию, и Вы должны реализовать функционал для изменения и удаления данных
 
-def print_operation_table(operation, num_rows, num_сolumns):
-    arr = [[operation(i, j) for i in range(1, num_rows + 1)] for j in range(1, num_сolumns + 1)]
-    for i in arr:
-        print(*[f"{x:>3}" for x in i])
+import os, re
 
 
-line = int(input("Введите количество строк: "))
-columns = int(input("Введите количество столбцов: "))
-print_operation_table(lambda x, y: x * y, line, columns)
+def phone_format(n):  # форматирование телефонного номера
+    n = n.removeprefix("+")
+    n = re.sub("[ ()-]", "", n)
+    return format(int(n[:-1]), ",").replace(",", "-") + n[-1]
+
+
+def printData(data):  # Функция вывода телефонной книги в консоль
+    phoneBook = []
+    splitLine = "=" * 49
+    print(splitLine)
+    print(" №  Lastname        Name          Phone Numbers")
+    print(splitLine)
+    personID = 1
+
+    for contact in data:
+        lastName, name, phone = contact.rstrip().split(",")
+        phoneBook.append(
+            {
+                "ID": personID,
+                "lastName": lastName,
+                "name": name,
+                "phone": phone_format(phone),
+            }
+        )
+        personID += 1
+
+    for contact in phoneBook:
+        personID, lastName, name, phone = contact.values()
+        print(f"{personID:>2}. {lastName:<15} {name:<10} -- {phone:<15}")
+
+    print(splitLine)
+
+
+def showContacts(fileName):  # Функция открытия телефонной книги
+    os.system("cls")
+    phoneBook = []
+    with open(fileName, "r", encoding="UTF-8") as file:
+        data = sorted(file.readlines())
+        printData(data)
+    input("\n--- press any key ---")
+
+
+def addContact(fileName):  # Функция добавления нового контакта в телефонную книгу
+    os.system("cls")
+    with open(fileName, "a", encoding="UTF-8") as file:
+        res = ""
+        res += input("Input a Surname of Contact: ") + ","
+        res += input("Input a Name of Contact: ") + ","
+        res += input("Input a Phone Number of Contact: ")
+
+        file.write(res + "\n")
+
+    input("\nContact was successfully added!\n--- press any key ---")
+
+
+def findContact(fileName):  # Функция поиска контактов в телефонной книге
+    os.system("cls")
+    target = input("Input Item of Contact for searching: ")
+    result = []
+    with open(fileName, "r", encoding="UTF-8") as file:
+        data = file.readlines()
+        for person in data:
+            if target in person:
+                result.append(person)
+                # break
+
+    if len(result) != 0:
+        printData(result)
+    else:
+        print(f"There is no Contact with this Item '{target}'.")
+
+    input("--- press any key ---")
+
+
+def changeContact(fileName):  # Функция изменения информации в контакте
+    os.system("cls")
+    phoneBook = []
+    with open(fileName, "r", encoding="UTF-8") as file:
+        data = sorted(file.readlines())
+        printData(data)
+
+        numberContact = int(
+            input("Input Number of Contact for changing or 0 for return Main Menu: ")
+        )
+        print(data[numberContact - 1].rstrip().split(","))
+        if numberContact != 0:
+            newLastName = input("Input new Lastname: ")
+            newName = input("Input new Name: ")
+            newPhone = input("Input new Phone: ")
+            data[numberContact - 1] = (
+                newLastName + "," + newName + "," + newPhone + "\n"
+            )
+            with open(fileName, "w", encoding="UTF-8") as file:
+                file.write("".join(data))
+                print("\nContact was successfully changed!")
+                input("\n--- press any key ---")
+        else:
+            return
+
+
+def deleteContact(fileName):  # Функция удаления контакта из телефонной книги
+    os.system("cls")
+    with open(fileName, "r+", encoding="UTF-8") as file:
+        data = sorted(file.readlines())
+        printData(data)
+
+        numberContact = int(
+            input("Input Number of Contact for deleting or 0 for return Main Menu: ")
+        )
+        if numberContact != 0:
+            print(f"Deleting record: {data[numberContact-1].rstrip().split(',')}\n")
+            data.pop(numberContact - 1)
+            with open(fileName, "w", encoding="UTF-8") as file:
+                file.write("".join(data))
+
+        else:
+            return
+
+    input("--- press any key ---")
+
+
+def drawInterface():  # Функция рисования интерфейса главного меню
+    print("#####   PHONE BOOK   #####")
+    print("=" * 26)
+    print(" [1] -- Show Contacts")
+    print(" [2] -- Add Contacts")
+    print(" [3] -- Find Contacts")
+    print(" [4] -- Change Contacts")
+    print(" [5] -- Delete Contacts")
+    print("\n [0] -- Exit")
+    print("=" * 26)
+
+
+def main(file_name):  # Функция реализации главного меню
+    while True:
+        os.system("cls")
+        drawInterface()
+        userChoice = int(input("Input a Number for 1 to 5 or 0 for Exit: "))
+
+        if userChoice == 1:
+            showContacts(file_name)
+        elif userChoice == 2:
+            addContact(file_name)
+        elif userChoice == 3:
+            findContact(file_name)
+        elif userChoice == 4:
+            changeContact(file_name)
+        elif userChoice == 5:
+            deleteContact(file_name)
+        elif userChoice == 0:
+            print("Thank you!")
+            return
+
+
+path = "phonebook.txt"
+
+main(path)
